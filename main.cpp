cd204e47df3cc623dc9f6f8c7896cdbcd3eb0e07
kikke88 <kikke88@yandex.ru>
2018-09-10 21:41:41 +0300

    before for rebuilding

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ void b_input(int size, double** arr, double** b_arr, int method) {
 	switch (method) {
 		
 		case 0: {
-			for (int i = 0; i < size; ++i) {
-				for (int j = 0; j < size; ++j) {
+			for (int i {0}; i < size; ++i) {
+				for (int j {0}; j < size; ++j) {
 					sum = sum + arr[i][j];
 				}
 			(*b_arr)[i] = sum;
@@ -18,8 +18,8 @@ void b_input(int size, double** arr, double** b_arr, int method) {
 		}
 
 		case 1: {
-			for (int i = 0; i < size; ++i) {
-				for (int j = 1; j < size; j = j + 2) {
+			for (int i {0}; i < size; ++i) {
+				for (int j {1}; j < size; j = j + 2) {
 					sum = sum + arr[i][j];
 				}
 			(*b_arr)[i] = sum;
@@ -29,8 +29,8 @@ void b_input(int size, double** arr, double** b_arr, int method) {
 		}
 
 		case 2: {
-			for (int i = 0; i < size; ++i) {
-				for (int j = 0; j < size; j = j + 2) {
+			for (int i {0}; i < size; ++i) {
+				for (int j {0}; j < size; j = j + 2) {
 					sum = sum + arr[i][j];
 				}
 			(*b_arr)[i] = sum;
@@ -69,7 +69,7 @@ void array_input(int& size, double*** arr, double** b_arr, std::function<double(
 	for (int i = 0; i < size; ++i) {
 		(*arr)[i] = new double[size]();/////////////////////////////
 	}
-	*b_arr = new double[size];
+	*b_arr = new double[size]();
 
 	for (int i = 0; i < size; ++i) {
 		for (int j = 0; j < size; ++j) {
@@ -78,13 +78,10 @@ void array_input(int& size, double*** arr, double** b_arr, std::function<double(
 	}
 	int method;
 	std::cin >> method; 
-	for (int i = 0; i < size; ++i) {
-
-		b_input(size, *arr, b_arr, method);
-		//(*b_arr)[i] = 0;///////////////////////////////////////
-	}
+	b_input(size, *arr, b_arr, method);	
 }
-void arr_pr(int size, double ** arr) {
+
+void array_print(int size, double ** arr) {
 	std::cout << "____________________________" << std::endl;
 	for (int i {0}; i < size; ++i) {
 		for (int j {0}; j < size; ++j) {
@@ -98,28 +95,27 @@ int main(int argc, char* argv[]) {
 	int array_size {0};///мб убрать инициализацию
 	double** array {nullptr};
 	double* b_array {nullptr};
+	
 	if (argc > 1) {
 		std::ifstream file {argv[1]};
 		array_input(array_size, &array, &b_array, file);
 		file.close();	
 	} else {
-		array_input(array_size, &array, &b_array, [](int i, int j) -> double { return i * j; });
+		array_input(array_size, &array, &b_array, [](int i, int j) -> double { return i + j + 1; });
 	}
-	for (int i {0}; i < array_size; ++i) {
-		for (int j {0}; j < array_size; ++j) {
-			std::cout << array[i][j] << "\t";
-		}
-		std::cout << std::endl;
-	}
+
+	array_print(array_size, array);
+
 	std::cout << "____________________________" << std::endl;
 	for (int j {0}; j < array_size; ++j) {
 			std::cout << b_array[j] << "\t";
 	}
 	std::cout << std::endl;
+
 	double** L = nullptr;
 	double** U = nullptr;
 
-	L = new double* [array_size];/////////////////////////нет инициализации нулями
+	L = new double* [array_size];
 	for (int i {0}; i < array_size; ++i) {
 		L[i] = new double[array_size]();
 	}
@@ -128,19 +124,18 @@ int main(int argc, char* argv[]) {
 	for (int i {0}; i < array_size; ++i) {
 		U[i] = new double[array_size]();
 	}
-	arr_pr(array_size, L);
-	arr_pr(array_size, U);
-	bool zzz;
-	std::cin >>zzz;
+	//	array_print(array_size, L);
+	//	array_print(array_size, U);
+
 	//algorithm realisation
 	for (int i {0}; i < array_size; ++i) {///потом этот фор можно будет совсем убрать
 		L[i][0] = array[i][0];
 	}
-	for (int j {0}; j < array_size; ++j) {
+	for (int j {1}; j < array_size; ++j) {/////
 		U[0][j] = array[0][j] / L[0][0];
 	}
-	arr_pr(array_size, L);
-	arr_pr(array_size, U);
+	//	array_print(array_size, L);
+	//	array_print(array_size, U);
 	//tmp
 	for (int counter {1}; counter < array_size - 1; ++counter) {
 		{//Lik
@@ -174,8 +169,8 @@ int main(int argc, char* argv[]) {
 		sum = sum + L[i][j] * U[j][k];
 	}
 	L[i][k] = array[i][k] - sum;
-	arr_pr(array_size, L);
-	arr_pr(array_size, U);
+	array_print(array_size, L);
+	array_print(array_size, U);
 
 
 
