d888601af1cad95bf780961a5fb0cac542455876
kikke88 <kikke88@yandex.ru>
2018-09-14 21:06:54 +0300

    eto win

--- ss.cpp
+++ ss.cpp
@@ -3,22 +3,40 @@
 #include <functional>
 #include <cmath>
 #include <utility>
+#include <cfloat>
 
-void one_dimensianal_array_print(const int size, const std::vector<double>& arr, std::ostream& stream) {
-	stream << "____________________________" << std::endl;
-	for(double num : arr) {
-		std::cout << num << "\t";
+void x_print(const int size, const std::vector<double>& x, std::ostream& stream, const std::vector<int>& indexes) {
+	stream << "<<x>>" << std::endl;
+	for (int i {0}; i < size; ++i) {
+		stream << "x[" << i << "]= " << x[indexes[i]] << std::endl;;
 	}
-	std::cout << std::endl;
+	stream << std::endl;
 }
 
-void two_dimensianal_array_print(const int size, const std::vector<std::vector<double>>& arr, std::ostream& stream) {
-	stream << "____________________________" << std::endl;
-	for(const std::vector<double>& sub_array : arr) {
-		for(const double num: sub_array) {
-			std::cout << num << "\t";
+void L_U_print(const int size, const std::vector<std::vector<double>>& arr, std::ostream& stream, const std::vector<int>& indexes) {
+	stream << "<<L>>" << std::endl;
+	for (int i {0}; i < size; ++i) {
+		for (int j {0}; j < size; ++j) {
+			if (i < j) {
+				stream << 0.0 << "\t";
+			} else {
+				stream << arr[indexes[i]][j] << "\t";
+			}
 		}
-	std::cout << std::endl;
+		stream << std::endl;
+	}
+	stream << "<<U>>" << std::endl;
+	for(int i {0}; i < size; ++i) {
+		for(int j {0}; j < size; ++j) {
+			if (i > j) {
+				stream << 0.0 << "\t";
+			} else if (i == j) {
+				stream << 1.0 << "\t";
+			} else {
+				stream << arr[indexes[i]][j] << "\t";
+			}
+		}
+		stream << std::endl;
 	}
 }
 
@@ -91,112 +109,115 @@ void array_input(int& size, std::vector<std::vector<double>>& arr, std::vector<d
 	b_input(size, arr, b_arr, method);	
 }
 
-void max_in_column(const int size, const int from_index, const int in_column,
+void max_column(const int size, const int line, const int column,
 								std::vector<int>& indexes, const std::vector<std::vector<double>>& arr) {
-	double max {std::abs(arr[indexes[from_index]][in_column])};
-	int index_of_max = from_index;
-	for (int i = from_index + 1; i < size; ++i) {
-		double cur_num {std::abs(arr[i][in_column])};
+	double max {std::abs(arr[indexes[line]][column])};
+	int line_of_max = line;
+	for (int i = line + 1; i < size; ++i) {
+		double cur_num {std::abs(arr[indexes[i]][column])};
 		if (cur_num > max) {
 			max = cur_num;
-			index_of_max = i;
+			line_of_max = i;
 		}
 	}
-	if (index_of_max != from_index) {
-		std::swap(indexes[from_index], indexes[index_of_max]);	
+	if (max < DBL_EPSILON) {
+		int ERROR = 1;
+		throw ERROR;
+	}
+	if (line_of_max != line) {
+		std::swap(indexes[line], indexes[line_of_max]);	
 	}
 }
 
-std::vector<int> main_func(const int size, std::vector<std::vector<double>>& arr, const std::vector<double>& b_arr, std::vector<double>& x_arr) { 	
-	std::vector<int> indexes{};
+std::vector<int> main_func(const int size, std::vector<std::vector<double>>& arr, const std::vector<double>& b_arr,
+								std::vector<double>& x_arr) { 	
+	std::vector<int> indexes;
 	for(int i{0}; i < size; ++i) {
 		indexes.push_back(i);
 	}
-	
+	max_column(size, 0, 0, indexes, arr);
 	for (int counter {0}; counter < size - 1;) {
 		if (counter == 0) {//Uik
 			for (int j {1}; j < size; ++j) {
-				arr[0][j] = arr[0][j] / arr[0][0];
-			}				
+				arr[indexes[0]][j] = arr[indexes[0]][j] / arr[indexes[0]][0];
+			}		
 		} else {
 			int i = counter;
-			for(int k {i + 1}; k < size; ++k) {
+			for (int k {i + 1}; k < size; ++k) {
 				double sum {0};
 				for (int j {0}; j < i; ++j) {
-					sum = sum + arr[i][j] * arr[j][k];
+					sum = sum + arr[indexes[i]][j] * arr[indexes[j]][k];
 				}
-				//eps
-				arr[i][k] = (arr[i][k] - sum) / arr[i][i];
+				arr[indexes[i]][k] = (arr[indexes[i]][k] - sum) / arr[indexes[i]][i];
 			}
 		}
 		++counter;
+		max_column(size, counter, counter, indexes, arr);
 		int k = counter;//Lik
 		for (int i {k}; i < size; ++i) {
 			double sum {0};
 			for (int j {0}; j < k; ++j) {
-				sum = sum + arr[i][j] * arr[j][k];
+				sum = sum + arr[indexes[i]][j] * arr[indexes[j]][k];
 			}
-			arr[i][k] = arr[i][k] - sum;
+			arr[indexes[i]][k] = arr[indexes[i]][k] - sum;
 		}
 	}
 	std::vector<double> y (size, 0);
 	for (int i {0}; i < size; ++i)	{
 		double sum {0};
 		for (int j {0}; j < i; ++j) {
-			sum = sum + y[j] * arr[i][j];
+			sum = sum + y[indexes[j]] * arr[indexes[i]][j];
 		}
-		y[i] = (b_arr[i] - sum) / arr[i][i];
+		y[indexes[i]] = (b_arr[indexes[i]] - sum) / arr[indexes[i]][i];
 	}
 	for (int i {size - 1}; i >= 0; --i)	{
 		double sum {0};
 		for (int j {i + 1}; j < size; ++j) {
-			sum = sum + x_arr[j] * arr[i][j];
+			sum = sum + x_arr[indexes[j]] * arr[indexes[i]][j];
 		}
-		x_arr[i] = y[i] - sum;
+		x_arr[indexes[i]] = y[indexes[i]] - sum;
 	}
 	return indexes;
 }
 
 double residual_calculation(const int size, const std::vector<std::vector<double>>& arr,
-								std::vector<double>& x_arr, std::vector<double>& b_arr) {
-	/*
+								std::vector<double>& x_arr, std::vector<double>& b_arr, const std::vector<int>& indexes) {
+	
 	for (int i {0}; i < size; ++i) {
-		double sum {x_arr[i]};
+		double sum {x_arr[indexes[i]]};
 		for (int j{i + 1}; j < size; ++j) {
-			sum = sum + x_arr[j] * U[i][j];
+			sum = sum + x_arr[indexes[j]] * arr[indexes[i]][j];
 		}
-		x_arr[i] = sum;
+		x_arr[indexes[i]] = sum;
 	}
 	for (int i {0}; i < size; ++i) {
 		double sum {0};
 		for (int j{0}; j < i + 1; ++j) {
-			sum = sum + x_arr[j] * L[i][j];
+			sum = sum + x_arr[indexes[j]] * arr[indexes[i]][j];
 		}
-		b_arr[i] = pow(sum - b_arr[i], 2);
+		b_arr[indexes[i]] = pow(sum - b_arr[indexes[i]], 2);
 	}
 	double res {0};
 	for (int i {0}; i < size; ++i)
 	{
-		res = res + b_arr[i];
+		res = res + b_arr[indexes[i]];
 	}
 	res = sqrt(res);
-	*/
 	return 0;
 }
 
-void result_output(const int size, const std::vector<std::vector<double>>& arr, std::vector<double>& x_arr, std::vector<double>& b_arr, bool bigsize) {	
+void result_output(const int size, const std::vector<std::vector<double>>& arr, std::vector<double>& x_arr,
+								std::vector<double>& b_arr, const std::vector<int>& indexes, bool bigsize) {	
 	if (bigsize) {
 		std::ofstream result_file {"result.txt"};
-		//two_dimensianal_array_print(size, L, result_file);
-		//two_dimensianal_array_print(size, U, result_file);
-		one_dimensianal_array_print(size, x_arr, result_file);
-		//result_file << residual_calculation(size, L, U, x_arr, b_arr) << std::endl;
+		L_U_print(size, arr, result_file, indexes);
+		x_print(size, x_arr, result_file, indexes);
+		result_file << residual_calculation(size, arr, x_arr, b_arr, indexes) << std::endl;
 		result_file.close();
 	} else {
-		//two_dimensianal_array_print(size, L, std::cout);
-		//two_dimensianal_array_print(size, U, std::cout);
-		one_dimensianal_array_print(size, x_arr, std::cout);
-		//std::cout << residual_calculation(size, L, U, x_arr, b_arr) << std::endl;
+		L_U_print(size, arr, std::cout, indexes);
+		x_print(size, x_arr, std::cout, indexes);
+		std::cout << residual_calculation(size, arr, x_arr, b_arr, indexes) << std::endl;
 	}
 }
 
@@ -209,18 +230,23 @@ int main(int argc, char* argv[]) {
 		array_input(array_size, array, b_array, file);
 		file.close();	
 	} else {
-		array_input(array_size, array, b_array, [](const int i, const int j, const int n) -> double { return (i + j + 1) / n; });
+		array_input(array_size, array, b_array, [](const int i, const int j, const int n) -> double { return (i + j + 1) / n; });//1.0
 	}
     std::vector<double> x (array_size, 0);
-    main_func(array_size, array, b_array, x); 
-    two_dimensianal_array_print(array_size, array, std::cout);
-    one_dimensianal_array_print(array_size, x, std::cout);
+    std::vector<int> indexes(main_func(array_size, array, b_array, x));
 /*
-	if (array_size <= 10) {
-		result_output(array_size, array, x, b_array, 0);
-	} else {
-		result_output(array_size, array, x, b_array, 1);
-	}
+    try {
+//    indexes = std::move(main_func(array_size, array, b_array, x));     	
+//    indexes = main_func(array_size, array, b_array, x);
+    } catch (const int) {
+    	std::cout << "degenerate matrix" << std::endl;
+    	return 1;
+    }
 */
+	if (array_size <= 10) {
+		result_output(array_size, array, x, b_array, indexes, 0);
+	} else {
+		result_output(array_size, array, x, b_array, indexes, 1);
+	}
 	return 0;
 }
